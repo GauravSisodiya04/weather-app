048952869a8bd57bc4d0c2319dcf37a235368d0d
GauravSisodiya04 <btech.gsdn@gmail.com>
2023-09-30 00:15:29 -0600

    fix: added changes for api

---
+++ src/js/main.js
@@ -0,0 +1,23 @@
+const API_KEY = "c07ac510ef0452db61a33b3cebaeecd0";
+const endpoint = "https://api.openweathermap.org/data/3.0/onecall?";
+const latitude = 20.8307;
+const longitude = 76.4505;
+
+window.onload = () => {
+    const getWeatherData = async function(){
+        const result = {};
+        const generateEndpointWithParameters = endpoint+"lat="+latitude+"&lon="+longitude+"&appid="+API_KEY;
+        console.log("My API URL becomes -->", generateEndpointWithParameters);
+        //const response = await fetch("https://api.weather.gov/points/20.8307,76.4505");
+        const response = await fetch("https://api.open-meteo.com/v1/forecast?latitude=19.8088&longitude=77.0471&hourly=temperature_2m");
+        const myJson = await response.json(); //extract JSON from the http response
+        console.log(myJson);
+    }
+    
+    const getWeatherDataButton = document.getElementById("get-weather-data-btn");
+    
+    getWeatherDataButton.addEventListener("click",getWeatherData);
+  };
+
+
+ 
